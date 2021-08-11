ea4c8c8395099411feaaaf7918898451e2c354de
Shawn Cochran <shawn.cochran31@gmail.com>
2021-08-11 16:33:04 -0600

    fixed server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dock-n-flock', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 module.exports = mongoose.connection;
