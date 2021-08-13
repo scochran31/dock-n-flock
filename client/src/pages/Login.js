af598a89263c8adec693ef941fd12363f9b1f596
Amanda Hoshor <hoshor.manda@gmail.com>
2021-08-12 19:48:16 -0600

    typedefs, resolvers, and utils folder

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+
+
 import {
     Flex,
     Box,
@@ -6,7 +8,9 @@ import {
     FormLabel,
     Input,
     Button,
-} from "@chakra-ui/react"
+} from "@chakra-ui/react";
+
+
 
 function Account() {
     const [emailAddress, setEmailAddress] = useState('')
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,38 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type Lot {
+    _id: ID
+    lotid: String
+    address: [String]
+    description: String
+    link: String
+    title: String
+  }
+
+  type Event {
+    _id: ID
+    eventId: String
+    category: [String]
+    description: String
+    link: String
+    title: String
+  }
+
   type User {
     _id: ID
     username: String
     email: String
+    lotCount: Int
+    eventCount: Int
+    savedLots: [Lot]
+    savedEvents: [Event]
   }
 
   type Auth {
     token: ID!
     user: User
   }
-
 
   type Query {
     me: User
@@ -22,6 +43,22 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+  input SavedLotInput {
+    lotid: String
+    address: [String]
+    description: String
+    link: String
+    title: String
+  }
+
+  input SavedEventInput {
+    eventId: String
+    category: [String]
+    description: String
+    link: String
+    title: String
   }
 `;
 
