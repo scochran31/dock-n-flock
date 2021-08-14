1e63ae17bf5e5062bc377b32b97569c2a399370c
Shawn Cochran <shawn.cochran31@gmail.com>
2021-08-13 21:35:02 -0600

    created react router dom, navlinks to all pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,33 @@
 import React from 'react'
-import { ChakraProvider } from "@chakra-ui/react"
+import { Box, ChakraProvider } from "@chakra-ui/react"
 import Nav from './components/Nav'
-// import Home from './pages/Home'
-// import Login from './pages/Login'
+import Home from './pages/Home'
+import Login from './pages/Login'
 import Search from './pages/Search'
+import Signup from './pages/Signup'
+import ParkingGarage from './pages/ParkingGarage'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from 'react-router-dom'
 
 function App() {
   return (
     <ChakraProvider>
-      <div>
+      <Router>
+        <Box>
         <Nav />
-      </div>
-      <main>
-        {/* <Home /> */}
-      {/* <Login /> */}
-        <Search />
-      </main>
+          <Switch>
+            <Route path='/login'><Login /></Route>
+            <Route path='/signup'><Signup /></Route>
+            <Route path='/parkingGarage'><ParkingGarage /></Route>
+            <Route path='/search'><Search /></Route>
+            <Route path='/'><Home /></Route>
+          </Switch>
+        </Box>
+      </Router>
     </ChakraProvider>
   )
 }
--- client/src/pages/ParkingGarage.js
+++ client/src/pages/ParkingGarage.js
@@ -1,1 +1,12 @@
-//Saved Events
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+
+function ParkingGarage() {
+    return (
+        <Box>
+            <Text fontSize='5xl'>This is the parking Garage: home to all of your favorite events!</Text>
+        </Box>
+    )
+}
+
+export default ParkingGarage
