e1407b30c35911206757d9e71a534a8a5b927ba3
cpesar <77510555+cpesar@users.noreply.github.com>
2021-08-16 13:19:41 -0600

    started signup layout

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,40 @@
-import { Box, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
 
-function Signup() {
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const Signup = () => {
+
+    const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+    const [addUser, { error }] = useMutation(ADD_USER);
+
+    const handleChange = event => {
+        const { name, value } = event.target;
+    
+        setFormState({
+          ...formState,
+          [name]: value,
+        });
+      };
+
+  const handleFormSubmit = async event => {
+    event.preventDefault();
+
+    // use try/catch to handle errors
+    try{
+      // execute addUser mutation and pass in varaible data from form
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+      
+    Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+
     return (
         <Box>
             <Text fontSize='5xl'>This is the Signup Page!</Text>
